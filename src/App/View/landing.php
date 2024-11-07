a5e33c4594c297b70ddf2d6298c798b6bc722ad2
ianmiguelsoler <ianmiguel@gmail.com>
2024-11-07 18:17:44 +0100

    terminado

--- src/App/View/landing.php
+++ src/App/View/landing.php
@@ -1,8 +1,8 @@
 <?php
 $titulo="ReserDAWtions";
 include_once "./environment.php";
-include_once "template/inicio.php";
-include_once "template/arribaNavegacion.php";
+include_once DIRECTORIO_VISTAS."template/inicio.php";
+include_once DIRECTORIO_VISTAS."template/arribaNavegacion.php";
 include_once "template/navegacion.php";
 include_once "template/slider.php";
 include_once "template/nosotros.php";
