a083d99842373f9606be653376430da15642023a
ianmiguelsoler <ianmiguel@gmail.com>
2024-11-07 17:15:27 +0100

    nuevos cambios

--- src/View/template/final.php
+++
@@ -1,7 +0,0 @@
-<script src="<?=DIRECTORIO_RECURSOS?>js/jquery.min.js"></script>
-<script src="<?=DIRECTORIO_RECURSOS?>js/bootstrap.bundle.min.js"></script>
-<script src="<?=DIRECTORIO_RECURSOS?>js/owl.carousel.min.js"></script>
-<script src="<?=DIRECTORIO_RECURSOS?>js/app.js"></script>
-</body>
-
-</html>
--- src/index.php
+++ src/index.php
@@ -1,33 +1,49 @@
 <?php
 
-//include_once "View/landing.php";
+include_once "environment.php";
+include_once "vendor/autoload.php";
 
-include_once "Router/Router.php";
-include_once "Controller/UsuarioController.php";
-use Router\Router;
-use Controller\UsuarioController;
-
-//echo "estoy en el index";
+use App\Router\Router;
+use App\Controller\UsuarioController;
+use App\Controller\ClienteController;
 
 $router = new Router();
 
-//Mostraría una landing page, una agina estática
+
+//Mostraria una landing page, una página estática
 $router->addRoute('get','/',function(){
-    include_once "View/landing.php";
+    include_once DIRECTORIO_VISTAS."landing.php";
 });
-$router->addRoute('GET','/about',function(){
-    include_once "View/about.php";
+$router->addRoute('get','/about',function(){
+    include_once DIRECTORIO_VISTAS."about.php";
 });
 
 //Rutas enlazadas a controladores, lógica de la aplicación
-$router->addRoute('GET','/users',[\Controller\UsuarioController::class,'index']);
-$router->addRoute('GET','/users/create',[\Controller\UsuarioController::class,'create']);
-$router->addRoute('POST','/users',[\Controller\UsuarioController::class,'store']);
-$router->addRoute('GET','/users/{id}/edit',[\Controller\UsuarioController::class,'edit']);
-$router->addRoute('POST','/users/{id}',[\Controller\UsuarioController::class,'update']);
-$router->addRoute('GET','/users/{id}',[\Controller\UsuarioController::class,'show']);
-$router->addRoute('DELETE','/users/{id}',[\Controller\UsuarioController::class,'destroy']);
+//Usuarios
+$router->addRoute('get','/users',[\App\Controller\UsuarioController::class,'index']);
+$router->addRoute('get','/users/create',[\App\Controller\UsuarioController::class,'create']);
+$router->addRoute('post','/users',[\App\Controller\UsuarioController::class,'store']);
+$router->addRoute('get','/users/{id}/edit',[\App\Controller\UsuarioController::class,'edit']);
+$router->addRoute('put','/users/{id}',[\App\Controller\UsuarioController::class,'update']);
+$router->addRoute('get','/users/{id}',[\App\Controller\UsuarioController::class,'show']);
+$router->addRoute('delete','/users/{id}',[\App\Controller\UsuarioController::class,'destroy']);
 
-//var_dump($_SERVER);
+//Clientes
+$router->addRoute('get','/clients',[\App\Controller\ClienteController::class,'index']);
+$router->addRoute('get','/clients/create',[\App\Controller\ClienteController::class,'create']);
+$router->addRoute('post','/clients',[\App\Controller\ClienteController::class,'store']);
+$router->addRoute('get','/clients/{id}/edit',[\App\Controller\ClienteController::class,'edit']);
+$router->addRoute('put','/clients/{id}',[\App\Controller\ClienteController::class,'update']);
+$router->addRoute('get','/clients/{id}',[\App\Controller\ClienteController::class,'show']);
+$router->addRoute('delete','/clients/{id}',[\App\Controller\ClienteController::class,'destroy']);
+
+//Reservas
+$router->addRoute('get','/bookings',[\App\Controller\ReservasController::class,'index']);
+$router->addRoute('get','/bookings/create',[\App\Controller\ReservasController::class,'create']);
+$router->addRoute('post','/bookings',[\App\Controller\ReservasController::class,'store']);
+$router->addRoute('get','/bookings/{id}/edit',[\App\Controller\ReservasController::class,'edit']);
+$router->addRoute('put','/bookings/{id}',[\App\Controller\ReservasController::class,'update']);
+$router->addRoute('get','/bookings/{id}',[\App\Controller\ReservasController::class,'show']);
+$router->addRoute('delete','/bookings/{id}',[\App\Controller\ReservasController::class,'destroy']);
 
 $router->resolver($_SERVER['REQUEST_METHOD'],$_SERVER['REQUEST_URI']);
